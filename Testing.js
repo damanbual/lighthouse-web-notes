ac5e872872a3a0da3030e0f89dd2207b6ab98c35
Daman Bual <damanbual@gmail.com>
2024-05-27 16:52:08 -0700

    followed along with Pedro's class May 27

--- Testing.js
+++ Testing.js
@@ -1,1 +1,25 @@
 
+// May 27 Class notes
+
+// Declare Function
+const assert = require('assert');
+
+function sayHello() {
+  return 'hello there!';
+}
+
+// Calling Function
+const hello = sayHello();
+console.log(hello);
+
+// Manual teting
+// console.assert (value to compare, message in case if falsy)
+// console.assert(true === true, 'is true!');
+// console.assert(true === false, 'not equal');
+// console.assert(sayHello() === 'hello there', 'not equal');
+
+// Using node:assert, assert.equal(actual, expected)
+assert.equal(1, 1);
+// assert.equal(1, 2);
+assert.strictEqual(1, '1');
+
